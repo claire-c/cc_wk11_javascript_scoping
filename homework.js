5adcf7ff6edd5653344dcf90d970a1fea2bcf814
Claire <cconnachan@gmail.com>
2018-05-14 20:06:12 +0100

    wrong again, but I believe it's because I missed the block. A fun homework

--- homework.js
+++ homework.js
@@ -1,134 +1,134 @@
-// const scenario = {
-//   murderer: 'Miss Scarlet',
-//   room: 'Library',
-//   weapon: 'Rope'
-// };
-//
-// const declareMurderer = function() {
-//   return `The murderer is ${scenario.murderer}.`;
-// }
-//
-// const verdict = declareMurderer();
-// console.log(verdict);
-//
-// // The console should print out Miss Scarlet as the murderer.
-//
-// const murderer = 'Professor Plum';
-//
-// const changeMurderer = function() {
-//   murderer = 'Mrs. Peacock';
-// }
-//
-// const delcareMurderer = function() {
-//   return `The murderer is ${murderer}.`;
-// }
-//
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
-//
-// // This should run an error, as you cannot change the murderer - it's been set to a const.
-//
-// let murderer = 'Professor Plum';
-//
-// const declareMurderer = function() {
-//   let murderer = 'Mrs. Peacock';
-//   return `The murderer is ${murderer}.`;
-// }
-//
-// const firstVerdict = declareMurderer();
-// console.log('First Verdict: ', firstVerdict);
-//
-// const secondVerdict = `The murderer is ${murderer}.`;
-// console.log('Second Verdict: ', secondVerdict);
-//
-// // This should return the first time with Mrs Peacock and the second time with Professor Plum.
-//
-// let suspectOne = 'Miss Scarlet';
-// let suspectTwo = 'Professor Plum';
-// let suspectThree = 'Mrs. Peacock';
-//
-// const declareAllSuspects = function() {
-//   let suspectThree = 'Colonel Mustard';
-//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
-// }
-//
-// const suspects = declareAllSuspects();
-// console.log(suspects);
-// console.log(`Suspect three is ${suspectThree}.`);
-//
-// // This should return Miss Scarlet, Professor Plum and Colonel Mustard.
-// // Then it should return "Suspect three is Mrs Peacock"
-//
-// const scenario = {
-//   murderer: 'Miss Scarlet',
-//   room: 'Kitchen',
-//   weapon: 'Candle Stick'
-// };
-//
-// const changeWeapon = function(newWeapon) {
-//   scenario.weapon = newWeapon;
-// }
-//
-// const declareWeapon = function() {
-//   return `The weapon is the ${scenario.weapon}.`;
-// }
-//
-// changeWeapon('Revolver');
-// const verdict = declareWeapon();
-// console.log(verdict);
-//
-// // I think this is going to allow the weapon change. It will say the weapon is a revolver.
-//
-// let murderer = 'Colonel Mustard';
-//
-// const changeMurderer = function() {
-//   murderer = 'Mr. Green';
-//
-//   const plotTwist = function() {
-//     murderer = 'Mrs. White';
-//   }
-//
-//   plotTwist();
-// }
-//
-// const declareMurderer = function () {
-//   return `The murderer is ${murderer}.`;
-// }
-//
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
-//
-// // I think this is going to return Mr Green.
-// // No! I was wrong. It was Mrs White. That's because I read the code wrong and thought the plot twist was a separate function - it's actually inside changeMurderer.
-//
-//
-// let murderer = 'Professor Plum';
-//
-// const changeMurderer = function() {
-//   murderer = 'Mr. Green';
-//
-//   const plotTwist = function() {
-//     let murderer = 'Colonel Mustard';
-//
-//     const unexpectedOutcome = function() {
-//       murderer = 'Miss Scarlet';
-//     }
-//
-//     unexpectedOutcome();
-//   }
-//
-//   plotTwist();
-// }
-//
-// const declareMurderer = function() {
-//   return `The murderer is ${murderer}.`;
-// }
-//
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The console should print out Miss Scarlet as the murderer.
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const delcareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// This should run an error, as you cannot change the murderer - it's been set to a const.
+
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// This should return the first time with Mrs Peacock and the second time with Professor Plum.
+
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+// This should return Miss Scarlet, Professor Plum and Colonel Mustard.
+// Then it should return "Suspect three is Mrs Peacock"
+
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+
+// I think this is going to allow the weapon change. It will say the weapon is a revolver.
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// I think this is going to return Mr Green.
+// No! I was wrong. It was Mrs White. That's because I read the code wrong and thought the plot twist was a separate function - it's actually inside changeMurderer.
+
+
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 // Oh, not sure. There is a global variable here. I think it will return Miss Scarlet.
 // Nope, wrong again. Now it's Mr Green?! Is it because it's a global variable?
@@ -169,3 +169,19 @@ const verdict = declareWeapon();
 console.log(verdict);
 
 // My brain hurts. I think this weapon is now the candle stick??
+
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// I feel like I am being tricked here... I think it will be Mrs Peacock...
+// LOL. I was wrong. It's Professor Plum. It's because of the block. I see now. 
