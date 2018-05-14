69a1aa0d379247f01ab748e245752246a5740073
Claire <cconnachan@gmail.com>
2018-05-14 19:50:09 +0100

    my first wrong guess - because I read the function incorrecly. A function inside a function

--- homework.js
+++ homework.js
@@ -1,61 +1,61 @@
-// const scenario = {
-//   murderer: 'Miss Scarlet',
-//   room: 'Library',
-//   weapon: 'Rope'
-// };
-//
-// const declareMurderer = function() {
-//   return `The murderer is ${scenario.murderer}.`;
-// }
-//
-// const verdict = declareMurderer();
-// console.log(verdict);
-//
-// // The console should print out Miss Scarlet as the murderer.
-//
-// const murderer = 'Professor Plum';
-//
-// const changeMurderer = function() {
-//   murderer = 'Mrs. Peacock';
-// }
-//
-// const delcareMurderer = function() {
-//   return `The murderer is ${murderer}.`;
-// }
-//
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
-//
-// // This should run an error, as you cannot change the murderer - it's been set to a const.
-//
-// let murderer = 'Professor Plum';
-//
-// const declareMurderer = function() {
-//   let murderer = 'Mrs. Peacock';
-//   return `The murderer is ${murderer}.`;
-// }
-//
-// const firstVerdict = declareMurderer();
-// console.log('First Verdict: ', firstVerdict);
-//
-// const secondVerdict = `The murderer is ${murderer}.`;
-// console.log('Second Verdict: ', secondVerdict);
-//
-// // This should return the first time with Mrs Peacock and the second time with Professor Plum.
-//
-// let suspectOne = 'Miss Scarlet';
-// let suspectTwo = 'Professor Plum';
-// let suspectThree = 'Mrs. Peacock';
-//
-// const declareAllSuspects = function() {
-//   let suspectThree = 'Colonel Mustard';
-//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
-// }
-//
-// const suspects = declareAllSuspects();
-// console.log(suspects);
-// console.log(`Suspect three is ${suspectThree}.`);
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The console should print out Miss Scarlet as the murderer.
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const delcareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// This should run an error, as you cannot change the murderer - it's been set to a const.
+
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// This should return the first time with Mrs Peacock and the second time with Professor Plum.
+
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
 
 // This should return Miss Scarlet, Professor Plum and Colonel Mustard.
 // Then it should return "Suspect three is Mrs Peacock"
@@ -79,3 +79,26 @@ const verdict = declareWeapon();
 console.log(verdict);
 
 // I think this is going to allow the weapon change. It will say the weapon is a revolver.
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// I think this is going to return Mr Green.
+// No! I was wrong. It was Mrs White. That's because I read the code wrong and thought the plot twist was a separate function - it's actually inside changeMurderer. 
